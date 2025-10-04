ae83fdbf8ddc9047d119e68a7d258a19b32bfebd
Nobbertins <ayaandhuka@gmail.com>
2025-10-04 12:49:23 -0500

    first commit

---
+++ STM32CubeIDE/Debug/Application/User/TouchGFX/App/app_touchgfx.d
@@ -0,0 +1,4 @@
+Application/User/TouchGFX/App/app_touchgfx.o: \
+ C://TouchGFXProjects/TestApp/TouchGFX/App/app_touchgfx.c \
+ C://TouchGFXProjects/TestApp/TouchGFX/App/app_touchgfx.h
+C://TouchGFXProjects/TestApp/TouchGFX/App/app_touchgfx.h:
